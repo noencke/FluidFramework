noencke/FluidFramework

commit 8ba75c508ff2370f3de0c9f63390f90a12d9bca2

feat(sequence): initial implementation of interval stickiness (#15423)
Builds on top of https://github.com/microsoft/FluidFramework/pull/15247
in order to implement interval stickiness

packages/dds/sequence/src/intervalCollection.ts
@@ -28,6 +28,7 @@ import {
 	maxReferencePosition,
 	createDetachedLocalReferencePosition,
 	DetachedReferencePosition,
+	SlidingPreference,
 } from "@fluidframework/merge-tree";
 import { ISequencedDocumentMessage } from "@fluidframework/protocol-definitions";
 import { LoggingError } from "@fluidframework/telemetry-utils";
@@ -39,6 +40,7 @@ import {
 	IValueOperation,
 	IValueType,
 	IValueTypeOperationValue,
+	SequenceOptions,
 } from "./defaultMapInterfaces";
 import { IInterval, IntervalConflictResolver, IntervalTree, IntervalNode } from "./intervalTree";
 
@@ -83,6 +85,7 @@ export interface ISerializedInterval {
 	end: number;
 	/** Interval type to create */
 	intervalType: IntervalType;
+	stickiness?: IntervalStickiness;
 	/** Any properties the interval has */
 	properties?: PropertySet;
 }
@@ -101,9 +104,11 @@ export type SerializedIntervalDelta = Omit<ISerializedInterval, "start" | "end" 
  *
  * Intervals are of the format:
  *
- * [start, end, sequenceNumber, intervalType, properties]
+ * [start, end, sequenceNumber, intervalType, properties, stickiness?]
  */
-export type CompressedSerializedInterval = [number, number, number, IntervalType, PropertySet];
+export type CompressedSerializedInterval =
+	| [number, number, number, IntervalType, PropertySet, IntervalStickiness]
+	| [number, number, number, IntervalType, PropertySet];
 
 /**
  * @internal
@@ -128,6 +133,7 @@ function decompressInterval(
 		sequenceNumber: interval[2],
 		intervalType: interval[3],
 		properties: { ...interval[4], [reservedRangeLabelsKey]: [label] },
+		stickiness: interval[5],
 	};
 }
 
@@ -138,7 +144,7 @@ function decompressInterval(
 function compressInterval(interval: ISerializedInterval): CompressedSerializedInterval {
 	const { start, end, sequenceNumber, intervalType, properties } = interval;
 
-	return [
+	const base: CompressedSerializedInterval = [
 		start,
 		end,
 		sequenceNumber,
@@ -147,6 +153,26 @@ function compressInterval(interval: ISerializedInterval): CompressedSerializedIn
 		// in the `label` field of the summary
 		{ ...properties, [reservedRangeLabelsKey]: undefined },
 	];
+
+	if (interval.stickiness !== undefined && interval.stickiness !== IntervalStickiness.END) {
+		base.push(interval.stickiness);
+	}
+
+	return base;
+}
+
+function startReferenceSlidingPreference(stickiness: IntervalStickiness): SlidingPreference {
+	// if any start stickiness, prefer sliding backwards
+	return (stickiness & IntervalStickiness.START) !== 0
+		? SlidingPreference.BACKWARD
+		: SlidingPreference.FORWARD;
+}
+
+function endReferenceSlidingPreference(stickiness: IntervalStickiness): SlidingPreference {
+	// if any end stickiness, prefer sliding forwards
+	return (stickiness & IntervalStickiness.END) !== 0
+		? SlidingPreference.FORWARD
+		: SlidingPreference.BACKWARD;
 }
 
 export interface ISerializableInterval extends IInterval {
@@ -193,8 +219,49 @@ export interface IIntervalHelpers<TInterval extends ISerializableInterval> {
 		intervalType: IntervalType,
 		op?: ISequencedDocumentMessage,
 		fromSnapshot?: boolean,
+		stickiness?: IntervalStickiness,
 	): TInterval;
 }
+
+/**
+ * Determines how an interval should expand when segments are inserted adjacent
+ * to the range it spans
+ *
+ * Note that interval stickiness is currently an experimental feature and must
+ * be explicitly enabled with the `intervalStickinessEnabled` flag
+ */
+export const IntervalStickiness = {
+	/**
+	 * Interval does not expand to include adjacent segments
+	 */
+	NONE: 0b00,
+
+	/**
+	 * Interval expands to include segments inserted adjacent to the start
+	 */
+	START: 0b01,
+
+	/**
+	 * Interval expands to include segments inserted adjacent to the end
+	 *
+	 * This is the default stickiness
+	 */
+	END: 0b10,
+
+	/**
+	 * Interval expands to include all segments inserted adjacent to it
+	 */
+	FULL: 0b11,
+} as const;
+
+/**
+ * Determines how an interval should expand when segments are inserted adjacent
+ * to the range it spans
+ *
+ * Note that interval stickiness is currently an experimental feature and must
+ * be explicitly enabled with the `intervalStickinessEnabled` flag
+ */
+export type IntervalStickiness = typeof IntervalStickiness[keyof typeof IntervalStickiness];
 
 /**
  * Serializable interval whose endpoints are plain-old numbers.
@@ -439,6 +506,7 @@ export class SequenceInterval implements ISerializableInterval {
 		public end: LocalReferencePosition,
 		public intervalType: IntervalType,
 		props?: PropertySet,
+		public readonly stickiness: IntervalStickiness = IntervalStickiness.END,
 	) {
 		this.propertyManager = new PropertiesManager();
 		this.properties = {};
@@ -500,6 +568,9 @@ export class SequenceInterval implements ISerializableInterval {
 		if (this.properties) {
 			serializedInterval.properties = this.properties;
 		}
+		if (this.stickiness !== IntervalStickiness.END) {
+			serializedInterval.stickiness = this.stickiness;
+		}
 
 		return serializedInterval;
 	}
@@ -514,6 +585,7 @@ export class SequenceInterval implements ISerializableInterval {
 			this.end,
 			this.intervalType,
 			this.properties,
+			this.stickiness,
 		);
 	}
 
@@ -621,6 +693,7 @@ export class SequenceInterval implements ISerializableInterval {
 		end: number,
 		op?: ISequencedDocumentMessage,
 		localSeq?: number,
+		stickiness: IntervalStickiness = IntervalStickiness.END,
 	) {
 		const getRefType = (baseType: ReferenceType): ReferenceType => {
 			let refType = baseType;
@@ -640,6 +713,7 @@ export class SequenceInterval implements ISerializableInterval {
 				op,
 				undefined,
 				localSeq,
+				startReferenceSlidingPreference(stickiness),
 			);
 			if (this.start.properties) {
 				startRef.addProperties(this.start.properties);
@@ -655,6 +729,7 @@ export class SequenceInterval implements ISerializableInterval {
 				op,
 				undefined,
 				localSeq,
+				endReferenceSlidingPreference(stickiness),
 			);
 			if (this.end.properties) {
 				endRef.addProperties(this.end.properties);
@@ -690,6 +765,7 @@ function createPositionReferenceFromSegoff(
 	op?: ISequencedDocumentMessage,
 	localSeq?: number,
 	fromSnapshot?: boolean,
+	slidingPreference?: SlidingPreference,
 ): LocalReferencePosition {
 	if (segoff.segment) {
 		const ref = client.createLocalReferencePosition(
@@ -697,6 +773,7 @@ function createPositionReferenceFromSegoff(
 			segoff.offset,
 			refType,
 			undefined,
+			slidingPreference,
 		);
 		return ref;
 	}
@@ -725,6 +802,7 @@ function createPositionReference(
 	op?: ISequencedDocumentMessage,
 	fromSnapshot?: boolean,
 	localSeq?: number,
+	slidingPreference?: SlidingPreference,
 ): LocalReferencePosition {
 	let segoff;
 	if (op) {
@@ -744,7 +822,16 @@ function createPositionReference(
 		);
 		segoff = client.getContainingSegment(pos, undefined, localSeq);
 	}
-	return createPositionReferenceFromSegoff(client, segoff, refType, op, localSeq, fromSnapshot);
+
+	return createPositionReferenceFromSegoff(
+		client,
+		segoff,
+		refType,
+		op,
+		localSeq,
+		fromSnapshot,
+		slidingPreference,
+	);
 }
 
 export function createSequenceInterval(
@@ -755,6 +842,7 @@ export function createSequenceInterval(
 	intervalType: IntervalType,
 	op?: ISequencedDocumentMessage,
 	fromSnapshot?: boolean,
+	stickiness: IntervalStickiness = IntervalStickiness.END,
 ): SequenceInterval {
 	let beginRefType = ReferenceType.RangeBegin;
 	let endRefType = ReferenceType.RangeEnd;
@@ -778,15 +866,38 @@ export function createSequenceInterval(
 		}
 	}
 
-	const startLref = createPositionReference(client, start, beginRefType, op, fromSnapshot);
-	const endLref = createPositionReference(client, end, endRefType, op, fromSnapshot);
+	const startLref = createPositionReference(
+		client,
+		start,
+		beginRefType,
+		op,
+		fromSnapshot,
+		undefined,
+		startReferenceSlidingPreference(stickiness),
+	);
+	const endLref = createPositionReference(
+		client,
+		end,
+		endRefType,
+		op,
+		fromSnapshot,
+		undefined,
+		endReferenceSlidingPreference(stickiness),
+	);
 	const rangeProp = {
 		[reservedRangeLabelsKey]: [label],
 	};
 	startLref.addProperties(rangeProp);
 	endLref.addProperties(rangeProp);
 
-	const ival = new SequenceInterval(client, startLref, endLref, intervalType, rangeProp);
+	const ival = new SequenceInterval(
+		client,
+		startLref,
+		endLref,
+		intervalType,
+		rangeProp,
+		stickiness,
+	);
 	return ival;
 }
 
@@ -1122,8 +1233,18 @@ export class LocalIntervalCollection<TInterval extends ISerializableInterval> {
 		end: number,
 		intervalType: IntervalType,
 		op?: ISequencedDocumentMessage,
+		stickiness: IntervalStickiness = IntervalStickiness.END,
 	): TInterval {
-		return this.helpers.create(this.label, start, end, this.client, intervalType, op);
+		return this.helpers.create(
+			this.label,
+			start,
+			end,
+			this.client,
+			intervalType,
+			op,
+			undefined,
+			stickiness,
+		);
 	}
 
 	public addInterval(
@@ -1132,8 +1253,9 @@ export class LocalIntervalCollection<TInterval extends ISerializableInterval> {
 		intervalType: IntervalType,
 		props?: PropertySet,
 		op?: ISequencedDocumentMessage,
+		stickiness: IntervalStickiness = IntervalStickiness.END,
 	) {
-		const interval: TInterval = this.createInterval(start, end, intervalType, op);
+		const interval: TInterval = this.createInterval(start, end, intervalType, op, stickiness);
 		if (interval) {
 			if (!interval.properties) {
 				interval.properties = createMap<any>();
@@ -1209,6 +1331,7 @@ export class LocalIntervalCollection<TInterval extends ISerializableInterval> {
 				ref.getOffset(),
 				ReferenceType.Transient,
 				ref.properties,
+				ref.slidingPreference,
 			);
 		};
 		if (interval instanceof SequenceInterval) {
@@ -1257,12 +1380,13 @@ class SequenceIntervalCollectionFactory
 	public load(
 		emitter: IValueOpEmitter,
 		raw: ISerializedInterval[] | ISerializedIntervalCollectionV2 = [],
+		options?: Partial<SequenceOptions>,
 	): IntervalCollection<SequenceInterval> {
 		const helpers: IIntervalHelpers<SequenceInterval> = {
 			compareEnds: compareSequenceIntervalEnds,
 			create: createSequenceInterval,
 		};
-		return new IntervalCollection<SequenceInterval>(helpers, true, emitter, raw);
+		return new IntervalCollection<SequenceInterval>(helpers, true, emitter, raw, options);
 	}
 
 	public store(
@@ -1297,7 +1421,15 @@ export class SequenceIntervalCollectionValueType
 
 const compareIntervalEnds = (a: Interval, b: Interval) => a.end - b.end;
 
-function createInterval(label: string, start: number, end: number, client: Client): Interval {
+function createInterval(
+	label: string,
+	start: number,
+	end: number,
+	client: Client,
+	intervalType?: IntervalType,
+	op?: ISequencedDocumentMessage,
+	fromSnapshot?: boolean,
+): Interval {
 	const rangeProp: PropertySet = {};
 
 	if (label && label.length > 0) {
@@ -1311,12 +1443,13 @@ class IntervalCollectionFactory implements IValueFactory<IntervalCollection<Inte
 	public load(
 		emitter: IValueOpEmitter,
 		raw: ISerializedInterval[] | ISerializedIntervalCollectionV2 = [],
+		options?: Partial<SequenceOptions>,
 	): IntervalCollection<Interval> {
 		const helpers: IIntervalHelpers<Interval> = {
 			compareEnds: compareIntervalEnds,
 			create: createInterval,
 		};
-		const collection = new IntervalCollection<Interval>(helpers, false, emitter, raw);
+		const collection = new IntervalCollection<Interval>(helpers, false, emitter, raw, options);
 		collection.attachGraph(undefined as any as Client, "");
 		return collection;
 	}
@@ -1542,6 +1675,7 @@ export class IntervalCollection<TInterval extends ISerializableInterval> extends
 		private readonly requiresClient: boolean,
 		private readonly emitter: IValueOpEmitter,
 		serializedIntervals: ISerializedInterval[] | ISerializedIntervalCollectionV2,
+		private readonly options: Partial<SequenceOptions> = {},
 	) {
 		super();
 
@@ -1685,7 +1819,7 @@ export class IntervalCollection<TInterval extends ISerializableInterval> extends
 		if (this.savedSerializedIntervals) {
 			for (const serializedInterval of this.savedSerializedIntervals) {
 				this.localCollection.ensureSerializedId(serializedInterval);
-				const { start, end, intervalType, properties } = serializedInterval;
+				const { start, end, intervalType, properties, stickiness } = serializedInterval;
 				const interval = this.helpers.create(
 					label,
 					start,
@@ -1694,6 +1828,7 @@ export class IntervalCollection<TInterval extends ISerializableInterval> extends
 					intervalType,
 					undefined,
 					true,
+					stickiness,
 				);
 				if (properties) {
 					interval.addProperties(properties);
@@ -1765,6 +1900,7 @@ export class IntervalCollection<TInterval extends ISerializableInterval> extends
 		end: number,
 		intervalType: IntervalType,
 		props?: PropertySet,
+		stickiness: IntervalStickiness = IntervalStickiness.END,
 	): TInterval {
 		if (!this.localCollection) {
 			throw new LoggingError("attach must be called prior to adding intervals");
@@ -1772,12 +1908,19 @@ export class IntervalCollection<TInterval extends ISerializableInterval> extends
 		if (intervalType & IntervalType.Transient) {
 			throw new LoggingError("Can not add transient intervals");
 		}
+		if (stickiness !== IntervalStickiness.END && !this.options.intervalStickinessEnabled) {
+			throw new UsageError(
+				"attempted to set interval stickiness without enabling `intervalStickinessEnabled` feature flag",
+			);
+		}
 
 		const interval: TInterval = this.localCollection.addInterval(
 			start,
 			end,
 			intervalType,
 			props,
+			undefined,
+			stickiness,
 		);
 
 		if (interval) {
@@ -1787,6 +1930,7 @@ export class IntervalCollection<TInterval extends ISerializableInterval> extends
 				properties: interval.properties,
 				sequenceNumber: this.client?.getCurrentSeq() ?? 0,
 				start,
+				stickiness,
 			};
 			const localSeq = this.getNextLocalSeq();
 			this.localSeqToSerializedInterval.set(localSeq, serializedInterval);
@@ -2239,6 +2383,7 @@ export class IntervalCollection<TInterval extends ISerializableInterval> extends
 					newStart,
 					interval.start.refType,
 					op,
+					startReferenceSlidingPreference(interval.stickiness),
 				);
 				if (props) {
 					interval.start.addProperties(props);
@@ -2256,6 +2401,7 @@ export class IntervalCollection<TInterval extends ISerializableInterval> extends
 					newEnd,
 					interval.end.refType,
 					op,
+					endReferenceSlidingPreference(interval.stickiness),
 				);
 				if (props) {
 					interval.end.addProperties(props);
@@ -2304,6 +2450,7 @@ export class IntervalCollection<TInterval extends ISerializableInterval> extends
 			serializedInterval.intervalType,
 			serializedInterval.properties,
 			op,
+			serializedInterval.stickiness,
 		);
 
 		if (interval) {

packages/dds/sequence/src/index.ts
@@ -15,7 +15,7 @@
  *
  * @packageDocumentation
  */
-export { IMapMessageLocalMetadata, IValueOpEmitter } from "./defaultMapInterfaces";
+export { IMapMessageLocalMetadata, IValueOpEmitter, SequenceOptions } from "./defaultMapInterfaces";
 export {
 	DeserializeCallback,
 	IIntervalCollectionEvent,
@@ -33,6 +33,7 @@ export {
 	ISerializedIntervalCollectionV2,
 	CompressedSerializedInterval,
 	SerializedIntervalDelta,
+	IntervalStickiness,
 } from "./intervalCollection";
 export { IInterval, IntervalConflictResolver } from "./intervalTree";
 export { ISharedSegmentSequenceEvents, SharedSegmentSequence } from "./sequence";

commit cb73414c732d35804e18d3c190fe06f6186924bd

fix(sequence): preserve original add() call shape in applyStashedOp (#27279)
## Summary
- `IntervalCollection.applyStashedOp` was reconstructing add/change ops
by always attaching the serialized `startSide`/`endSide` to numeric
positions. Serialized ops include sides unconditionally for
future-proofing, so this turned every pending interval add into a `{
pos, side: Side.Before }` shape — which `assertStickinessEnabled` treats
as a stickiness request, throwing `attempted to set interval stickiness
without enabling \`intervalStickinessEnabled\` feature flag` whenever a
pending state was rehydrated with the flag off.
- The fix preserves the original `add()`/`change()` call shape on replay
by dropping the redundant default `Side.Before` for numeric positions
before calling `add`/`change` from `applyStashedOp`.
`assertStickinessEnabled` (originally introduced in #15423) is unchanged
— the gate is still strictly shape-based, the call shape is just now
correct.
- Regresses behavior introduced in #19675, which wired `applyStashedOp`
to call `add`/`change` directly with `toSequencePlace(value.start,
value.startSide)`. `serialize()` has always emitted concrete sides, but
no stashed-op test covered the `intervalStickinessEnabled: false` path
until now.
- The `?? defaultSide` substitutions at
`intervalCollection.ts:1430-1431,1674-1675` are pure
literal-centralization — those paths (`ackChangeInterval`, `ackAdd`)
don't go through `assertStickinessEnabled`, so they're behaviorally
inert and separate from the `applyStashedOp` shape fix; bisect
accordingly.
- Adds round-trip coverage that serializes an interval and applies it as
a stashed op on a fresh client. Six cases total: add + change ×
{non-sticky/flag off, non-sticky/flag on, sticky/flag on}.

## Test plan
- [x] `pnpm -F @fluidframework/sequence test` — 1664 passing, including
the six new round-trip cases and the previously-flaky 100+ fuzz seeds
that broke when an earlier draft mutated frozen op values.

---------

packages/dds/merge-tree/src/index.ts
@@ -127,6 +127,7 @@ export {
 	type InteriorSequencePlace,
 	Side,
 	type SequencePlace,
+	defaultSide,
 	endpointPosAndSide,
 } from "./sequencePlace.js";
 export { SortedSet } from "./sortedSet.js";

packages/dds/merge-tree/src/sequencePlace.ts
@@ -53,6 +53,20 @@ export enum Side {
 }
 
 /**
+ * The {@link Side} a bare numeric `SequencePlace` resolves to when no side is
+ * explicitly provided. Exported so callers that need to detect "the caller
+ * didn't specify a side" (such as serialized op replay) can compare against
+ * this without hardcoding `Side.Before`.
+ *
+ * Note: this only governs the bare-numeric case. The `"start"` and `"end"`
+ * literals are normalized by `normalizePlace` with their own conventions
+ * (`"start"` → `Side.After`, `"end"` → `Side.Before`) and are unrelated to
+ * `defaultSide`.
+ * @internal
+ */
+export const defaultSide = Side.Before;
+
+/**
  * Returns the position and side of the start and end of a sequence.
  *
  * @legacy @beta
@@ -70,8 +84,8 @@ export function endpointPosAndSide(
 		typeof start === "number" || start === "start" || start === "end";
 	const endIsPlainEndpoint = typeof end === "number" || end === "start" || end === "end";
 
-	const startSide = startIsPlainEndpoint ? Side.Before : start?.side;
-	const endSide = endIsPlainEndpoint ? Side.Before : end?.side;
+	const startSide = startIsPlainEndpoint ? defaultSide : start?.side;
+	const endSide = endIsPlainEndpoint ? defaultSide : end?.side;
 
 	const startPos = startIsPlainEndpoint ? start : start?.pos;
 	const endPos = endIsPlainEndpoint ? end : end?.pos;
@@ -89,7 +103,7 @@ export function endpointPosAndSide(
  */
 export function normalizePlace(place: SequencePlace): InteriorSequencePlace {
 	if (typeof place === "number") {
-		return { pos: place, side: Side.Before };
+		return { pos: place, side: defaultSide };
 	}
 	if (place === "start") {
 		return { pos: -1, side: Side.After };

packages/dds/sequence/src/intervalCollection.ts
@@ -27,6 +27,7 @@ import {
 	refTypeIncludesFlag,
 	reservedRangeLabelsKey,
 	Side,
+	defaultSide,
 	endpointPosAndSide,
 	type ISegmentInternal,
 	createLocalReconnectingPerspective,
@@ -945,21 +946,30 @@ export class IntervalCollection
 	public applyStashedOp(op: IIntervalCollectionTypeOperationValue): void {
 		const { opName, value } = op;
 		const { id, properties } = getSerializedProperties(value);
+		// Serialized ops always include startSide/endSide for future-proofing,
+		// but the original add()/change() call may have used plain numeric
+		// positions. Drop the side when it equals the default so the replayed
+		// call matches the original shape — otherwise assertStickinessEnabled
+		// would treat a plain-number interval as sticky. Stashed-op values
+		// can be deep-frozen by upstream layers (e.g. test mocks), so this is
+		// computed into locals rather than mutating value.
+		const startSide = value.startSide === defaultSide ? undefined : value.startSide;
+		const endSide = value.endSide === defaultSide ? undefined : value.endSide;
 		switch (opName) {
 			case "add": {
 				this.add({
 					id,
 					// Todo: we should improve typing so we know add ops always have start and end
-					start: toSequencePlace(value.start, value.startSide),
-					end: toSequencePlace(value.end, value.endSide),
+					start: toSequencePlace(value.start, startSide),
+					end: toSequencePlace(value.end, endSide),
 					props: properties,
 				});
 				break;
 			}
 			case "change": {
 				this.change(id, {
-					start: toOptionalSequencePlace(value.start, value.startSide),
-					end: toOptionalSequencePlace(value.end, value.endSide),
+					start: toOptionalSequencePlace(value.start, startSide),
+					end: toOptionalSequencePlace(value.end, endSide),
 					props: properties,
 				});
 				break;
@@ -1427,8 +1437,8 @@ export class IntervalCollection
 				const { start, end, startSide, endSide } = serializedInterval;
 				newInterval = intervalToChange.modify(
 					"",
-					toOptionalSequencePlace(start, startSide ?? Side.Before),
-					toOptionalSequencePlace(end, endSide ?? Side.Before),
+					toOptionalSequencePlace(start, startSide ?? defaultSide),
+					toOptionalSequencePlace(end, endSide ?? defaultSide),
 					op,
 				);
 				if (isLatestInterval) {
@@ -1671,8 +1681,8 @@ export class IntervalCollection
 
 		const interval: SequenceIntervalClass = this.localCollection.addInterval(
 			id,
-			toSequencePlace(serializedInterval.start, serializedInterval.startSide ?? Side.Before),
-			toSequencePlace(serializedInterval.end, serializedInterval.endSide ?? Side.Before),
+			toSequencePlace(serializedInterval.start, serializedInterval.startSide ?? defaultSide),
+			toSequencePlace(serializedInterval.end, serializedInterval.endSide ?? defaultSide),
 			properties,
 			op,
 		);

packages/dds/sequence/src/test/intervalStashedOps.spec.ts
@@ -8,6 +8,7 @@
 import { strict as assert } from "node:assert";
 
 import { AttachState } from "@fluidframework/container-definitions";
+import { Side } from "@fluidframework/merge-tree/internal";
 import { IntervalType } from "@fluidframework/sequence-previous/internal";
 import {
 	MockContainerRuntimeFactory,
@@ -17,7 +18,7 @@ import {
 
 import type { ISequenceIntervalCollection } from "../intervalCollection.js";
 import type { IMapOperation } from "../intervalCollectionMap.js";
-import { IntervalOpType } from "../intervals/index.js";
+import { IntervalOpType, SequenceIntervalClass } from "../intervals/index.js";
 import { SharedStringFactory, type SharedString } from "../sequenceFactory.js";
 import { SharedStringClass } from "../sharedString.js";
 
@@ -165,6 +166,175 @@ describe("Interval Stashed Ops on client ", () => {
 			sharedString["applyStashedOp"](opArgs);
 			assertIntervals(sharedString, collection, [{ start: 0, end: 5 }]);
 			assert.equal(interval.properties.a, 2);
+		});
+	});
+
+	describe("round-trip via applyStashedOp", () => {
+		const label = "test";
+
+		function createSharedString(intervalStickinessEnabled: boolean): SharedString {
+			const runtime = new MockFluidDataStoreRuntime({ clientId: "rt" });
+			runtime.options = { intervalStickinessEnabled };
+			runtime.setAttachState(AttachState.Attached);
+			const factory = new MockContainerRuntimeFactory();
+			factory.createContainerRuntime(runtime);
+			const ss = new SharedStringClass(runtime, "ss", SharedStringFactory.Attributes);
+			ss.initializeLocal();
+			ss.connect({
+				deltaConnection: runtime.createDeltaConnection(),
+				objectStorage: new MockStorage(),
+			});
+			return ss;
+		}
+
+		function assertIntervalMatches(
+			scenario: string,
+			target: SharedString,
+			replayed: SequenceIntervalClass | undefined,
+			source: SharedString,
+			expected: SequenceIntervalClass,
+		): void {
+			assert(replayed !== undefined, `${scenario}: expected replayed interval`);
+			assert.equal(
+				target.localReferencePositionToPosition(replayed.start),
+				source.localReferencePositionToPosition(expected.start),
+				`${scenario}: start position mismatch`,
+			);
+			assert.equal(
+				target.localReferencePositionToPosition(replayed.end),
+				source.localReferencePositionToPosition(expected.end),
+				`${scenario}: end position mismatch`,
+			);
+			assert.equal(replayed.startSide, expected.startSide, `${scenario}: startSide mismatch`);
+			assert.equal(replayed.endSide, expected.endSide, `${scenario}: endSide mismatch`);
+			assert.equal(
+				replayed.stickiness,
+				expected.stickiness,
+				`${scenario}: stickiness mismatch`,
+			);
+		}
+
+		function assertAddRoundTrip(
+			intervalStickinessEnabled: boolean,
+			addArgs: Parameters<ISequenceIntervalCollection["add"]>[0],
+		): void {
+			const source = createSharedString(intervalStickinessEnabled);
+			const target = createSharedString(intervalStickinessEnabled);
+			source.insertText(0, "hello world");
+			target.insertText(0, "hello world");
+
+			const sourceCollection = source.getIntervalCollection(label);
+			const sourceInterval = sourceCollection.add(addArgs) as SequenceIntervalClass;
+
+			const targetCollection = target.getIntervalCollection(label);
+			target["applyStashedOp"]({
+				key: label,
+				type: "act",
+				value: { opName: IntervalOpType.ADD, value: sourceInterval.serialize() },
+			} satisfies IMapOperation);
+
+			const [replayed] = Array.from(targetCollection) as SequenceIntervalClass[];
+			assertIntervalMatches("add", target, replayed, source, sourceInterval);
+		}
+
+		function assertChangeRoundTrip(
+			intervalStickinessEnabled: boolean,
+			addArgs: Parameters<ISequenceIntervalCollection["add"]>[0],
+			changeArgs: Parameters<ISequenceIntervalCollection["change"]>[1],
+		): void {
+			const source = createSharedString(intervalStickinessEnabled);
+			const target = createSharedString(intervalStickinessEnabled);
+			source.insertText(0, "hello world");
+			target.insertText(0, "hello world");
+
+			const sourceCollection = source.getIntervalCollection(label);
+			const initial = sourceCollection.add(addArgs) as SequenceIntervalClass;
+			const id = initial.getIntervalId();
+
+			const targetCollection = target.getIntervalCollection(label);
+			target["applyStashedOp"]({
+				key: label,
+				type: "act",
+				value: { opName: IntervalOpType.ADD, value: initial.serialize() },
+			} satisfies IMapOperation);
+
+			const changed = sourceCollection.change(id, changeArgs) as SequenceIntervalClass;
+			target["applyStashedOp"]({
+				key: label,
+				type: "act",
+				value: {
+					opName: IntervalOpType.CHANGE,
+					value: changed.serializeDelta({ props: undefined, includeEndpoints: true }),
+				},
+			} satisfies IMapOperation);
+
+			const [replayed] = Array.from(targetCollection) as SequenceIntervalClass[];
+			assertIntervalMatches("change", target, replayed, source, changed);
+		}
+
+		describe("add", () => {
+			it("non-sticky interval with intervalStickinessEnabled disabled", () => {
+				assertAddRoundTrip(false, { start: 0, end: 5 });
+			});
+
+			it("non-sticky interval with intervalStickinessEnabled enabled", () => {
+				assertAddRoundTrip(true, { start: 0, end: 5 });
+			});
+
+			it("sticky interval with intervalStickinessEnabled enabled", () => {
+				assertAddRoundTrip(true, {
+					start: "start",
+					end: { pos: 5, side: Side.After },
+				});
+			});
+		});
+
+		describe("change", () => {
+			it("non-sticky endpoints with intervalStickinessEnabled disabled", () => {
+				assertChangeRoundTrip(false, { start: 0, end: 5 }, { start: 2, end: 7 });
+			});
+
+			it("non-sticky endpoints with intervalStickinessEnabled enabled", () => {
+				assertChangeRoundTrip(true, { start: 0, end: 5 }, { start: 2, end: 7 });
+			});
+
+			it("sticky endpoints with intervalStickinessEnabled enabled", () => {
+				assertChangeRoundTrip(
+					true,
+					{ start: "start", end: { pos: 5, side: Side.After } },
+					{ start: "start", end: { pos: 7, side: Side.After } },
+				);
+			});
+		});
+
+		describe("stickiness gate", () => {
+			// Symmetric negative-guard: a genuinely sticky serialized op must
+			// still trip assertStickinessEnabled when the target has the flag off.
+			// Guards against a future "always strip sides on replay" simplification
+			// silently weakening the feature gate.
+			it("sticky serialized op replayed against intervalStickinessEnabled=false throws", () => {
+				const source = createSharedString(true);
+				const target = createSharedString(false);
+				source.insertText(0, "hello world");
+				target.insertText(0, "hello world");
+
+				const sourceCollection = source.getIntervalCollection(label);
+				const sticky = sourceCollection.add({
+					start: "start",
+					end: { pos: 5, side: Side.After },
+				}) as SequenceIntervalClass;
+				target.getIntervalCollection(label);
+
+				assert.throws(
+					() =>
+						target["applyStashedOp"]({
+							key: label,
+							type: "act",
+							value: { opName: IntervalOpType.ADD, value: sticky.serialize() },
+						} satisfies IMapOperation),
+					/intervalStickinessEnabled/,
+				);
+			});
 		});
 	});
 });